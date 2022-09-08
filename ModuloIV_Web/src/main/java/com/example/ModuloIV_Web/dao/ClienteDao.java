6056899e2ff64cd375fe9cd25db6eed05e30185d
marianamoreira <marianaxmoreira@gmail.com>
2022-09-08 13:41:02 -0300

    Delete ModuloIV_Web directory

--- ModuloIV_Web/src/main/java/com/example/ModuloIV_Web/dao/ClienteDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.ModuloIV_Web.dao;
-
-import com.example.ModuloIV_Web.Models.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteDao extends JpaRepository<Cliente, Long> {
-
-}
-
--- ModuloIV_Web/src/main/java/com/example/ModuloIV_Web/dao/ProdutoDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.ModuloIV_Web.dao;
-
-import com.example.ModuloIV_Web.Models.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoDao extends JpaRepository<Produto,Long> {
-}
